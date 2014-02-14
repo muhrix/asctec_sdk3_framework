d54e4db99db40007a3242f8645cdd6d5a02848ae
Murilo FM <muhrix+jupiter@gmail.com>
2014-02-14 21:01:40 -0200

    Modified hlp_node.cpp to include ACI wrapper class AciRemote

--- asctec_hlp_interface/src/hlp_node.cpp
+++ asctec_hlp_interface/src/hlp_node.cpp
@@ -8,19 +8,19 @@
  */
 
 #include <ros/ros.h>
-#include "asctec_hlp_interface/SerialComm.h"
+#include "asctec_hlp_interface/AciRemote.h"
 
 int main(int argc, char* argv[]) {
 	ros::init(argc, argv, "hlp_node");
 	ros::NodeHandle n;
 
-	SerialComm serial;
-	if (serial.openPort() < 0) {
-		return -1;
-	}
-	else {
-		serial.closePort();
-	}
+	AciRemote::AciRemote hlp(std::string("/dev/ttyUSB0"));
+	//if (hlp.openPort() < 0) {
+	//	return -1;
+	//}
+	//else {
+	//	serial.closePort();
+	//}
 
 	return 0;
 }
