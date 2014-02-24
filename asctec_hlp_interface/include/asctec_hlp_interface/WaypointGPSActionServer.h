41909c16c5b513908f85922cee2d6ef9dde95cde
Murilo FM <muhrix+jupiter@gmail.com>
2014-02-24 20:05:49 -0300

    WaypointGPS ROS action server implemented

--- asctec_hlp_interface/include/asctec_hlp_interface/WaypointGPSActionServer.h
+++ asctec_hlp_interface/include/asctec_hlp_interface/WaypointGPSActionServer.h
@@ -10,27 +10,76 @@
 #ifndef WAYPOINTGPSACTIONSERVER_H_
 #define WAYPOINTGPSACTIONSERVER_H_
 
+#include <boost/function.hpp>
+
 #include <ros/ros.h>
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/Quaternion.h>
+#include <tf/transform_datatypes.h>
+#include <geographic_msgs/GeoPoint.h>
+#include <geographic_msgs/GeoPose.h>
 #include <actionlib/server/simple_action_server.h>
-#include <asctec_hlp_comm/WaypointGPSAction.h>
+#include "asctec_hlp_comm/WaypointGPSAction.h"
+#include "asctec_hlp_comm/GeofencePoint.h"
+#include "asctec_hlp_comm/GeofenceSrv.h"
 
 #include "asctec_hlp_interface/AsctecSDK3.h"
+
+namespace Waypoint {
+	namespace Action {
+		typedef enum {SUCCEEDED = 0, RUNNING, PREEMPTED, ABORTED, OUT_OF_GEOFENCE, VALID} action_t;
+	}
+	namespace Status {
+		typedef enum {REACHED_POS = 0, REACHED_POS_TIME, WITHIN_20M, PILOT_ABORT } status_t;
+	}
+}
 
 class WaypointGPSActionServer {
 public:
 	WaypointGPSActionServer(const std::string&);
 	~WaypointGPSActionServer();
 
-	void executeCallback(const asctec_hlp_comm::WaypointGPSGoalConstPtr&);
+	// getters
+	double getWaypointMaxSpeed() const;
+	double getWaypointPositionAccuracy() const;
+	double getWaypointTimeout() const;
 
-protected:
+	// setters
+	void setWaypointMaxSpeed(const double);
+	void setWaypointPositionAccuracy(const double);
+	void setWaypointTimeout(const double);
+
+//protected:
+private:
 	ros::NodeHandle n_;
 	std::string action_name_;
-	actionlib::SimpleActionServer<asctec_hlp_comm::WaypointGPSAction> as_;
-	asctec_hlp_comm::WaypointGPSActionFeedback feedback_;
-	asctec_hlp_comm::WaypointGPSActionResult result_;
+	std::string geofence_srv_name_;
 
-	struct WAYPOINT waypoint_;
+	ros::ServiceServer geofence_srv_;
+
+	actionlib::SimpleActionServer<asctec_hlp_comm::WaypointGPSAction> as_;
+	asctec_hlp_comm::WaypointGPSFeedback feedback_;
+	asctec_hlp_comm::WaypointGPSResult result_;
+
+	double waypt_max_speed_;
+	double waypt_pos_acc_;
+	double waypt_timeout_;
+	boost::shared_ptr<geographic_msgs::GeoPose> waypoint_;
+	std::vector<boost::shared_ptr<geographic_msgs::GeoPoint> > geofence_;
+
+//private:
+	void GpsWaypointAction(const asctec_hlp_comm::WaypointGPSGoalConstPtr&);
+
+	bool geofenceServiceCallback(asctec_hlp_comm::GeofenceSrv::Request&,
+			asctec_hlp_comm::GeofenceSrv::Response&);
+
+	int verifyGeofence();
+	Waypoint::Action::action_t verifyGpsWaypoint(const asctec_hlp_comm::WaypointGPSGoalConstPtr&);
+
+	boost::function<void (const asctec_hlp_comm::WaypointGPSGoalConstPtr&)>
+		sendGpsWaypointToHlp;
+	boost::function<void (unsigned short&, double&)> fetchWayptNavStatus;
+	boost::function<void (asctec_hlp_comm::WaypointGPSResult&)> fetchWayptResultPose;
 };
 
 #endif /* WAYPOINTGPSACTIONSERVER_H_ */
--- asctec_hlp_interface/src/WaypointGPSActionServer.cpp
+++ asctec_hlp_interface/src/WaypointGPSActionServer.cpp
@@ -12,9 +12,13 @@
 
 WaypointGPSActionServer::WaypointGPSActionServer(const std::string& name):
 	n_("~"),
-	as_(n_, name, boost::bind(&WaypointGPSActionServer::executeCallback, this, _1), false),
-	action_name_(name) {
-	// subscribe to the data topic of interest
+	as_(n_, name, boost::bind(&WaypointGPSActionServer::GpsWaypointAction, this, _1), false),
+	action_name_(name),
+	waypt_max_speed_(100.0),
+	waypt_pos_acc_(3.0),
+	waypt_timeout_(10000) {
+	n_.param("geofence_service", geofence_srv_name_, std::string("geofence"));
+	// start action server
 	as_.start();
 }
 
@@ -22,17 +26,129 @@ WaypointGPSActionServer::~WaypointGPSActionServer() {
 
 }
 
-void WaypointGPSActionServer::executeCallback(
+
+
+//-------------------------------------------------------
+//	Public member functions
+//-------------------------------------------------------
+
+// getters
+double WaypointGPSActionServer::getWaypointMaxSpeed() const {
+	return waypt_max_speed_;
+}
+
+double WaypointGPSActionServer::getWaypointPositionAccuracy() const {
+	return waypt_pos_acc_;
+}
+
+double WaypointGPSActionServer::getWaypointTimeout() const {
+	return waypt_timeout_;
+}
+
+// setters
+void WaypointGPSActionServer::setWaypointMaxSpeed(const double s) {
+	waypt_max_speed_ = s;
+}
+
+void WaypointGPSActionServer::setWaypointPositionAccuracy(const double a) {
+	waypt_pos_acc_ = a;
+}
+
+void WaypointGPSActionServer::setWaypointTimeout(const double t) {
+	waypt_timeout_ = t;
+}
+
+
+
+//-------------------------------------------------------
+//	Private member functions
+//-------------------------------------------------------
+
+void WaypointGPSActionServer::GpsWaypointAction(
 		const asctec_hlp_comm::WaypointGPSGoalConstPtr& goal) {
+	// helper variables
+	ros::Rate rate(1);
+	Waypoint::Action::action_t running;
 
-	bool success = true;
+	double roll, pitch, yaw;
+	unsigned short nav_status;
+	double dist_to_goal;
 
-	// assign the newly defined GPS waypoint to waypoint_ data structure
+	tf::Quaternion q;
+	tf::quaternionMsgToTF(goal->geo_pose.orientation, q);
+	tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
 
+	running = verifyGpsWaypoint(goal);
 
+	if (running == Waypoint::Action::VALID) {
+		ROS_INFO_STREAM("Flying to GPS waypoint (" << goal->geo_pose.position.latitude << ", "
+				<< goal->geo_pose.position.longitude << ", "
+				<< goal->geo_pose.position.altitude << ", "
+				<< (yaw * 180.0 / M_PI) << ")");
+
+		// assign the newly defined GPS waypoint to ACI Engine
+		sendGpsWaypointToHlp(goal);
+		running = Waypoint::Action::RUNNING;
+	}
 	// loop reading GPS waypoint navigation status variables
-	// check whether current waypoint should be preempted
-	// set and publish feedback at each loop iteration
+	while (running == Waypoint::Action::RUNNING) {
+		// check if preempt has been requested by client (and if ROS is running ok)
+		if (as_.isPreemptRequested() || !ros::ok()) {
+			running = Waypoint::Action::PREEMPTED;
+			break;
+		}
+		// fetch waypoint navigation status from HLP
+		fetchWayptNavStatus(nav_status, dist_to_goal);
+		if (nav_status & 0x08 == static_cast<unsigned short>(Waypoint::Status::PILOT_ABORT)) {
+			running = Waypoint::Action::ABORTED;
+			break;
+		}
+		else if ((nav_status & 0x01 ==
+						static_cast<unsigned short>(Waypoint::Status::REACHED_POS))
+				|| (nav_status & 0x02 ==
+						static_cast<unsigned short>(Waypoint::Status::REACHED_POS))) {
+			// set result
+			fetchWayptResultPose(result_);
+			result_.status = nav_status;
+			running = Waypoint::Action::SUCCEEDED;
+			break;
+		}
+		else {
+			// set and publish feedback at each loop iteration
+			feedback_.distance = dist_to_goal;
+			feedback_.status = nav_status;
+			as_.publishFeedback(feedback_);
+		}
+
+		rate.sleep();
+	}
+	if (running == Waypoint::Action::OUT_OF_GEOFENCE) {
+		ROS_INFO_STREAM(action_name_ << ": Invalid waypoint (out of geofence)");
+		as_.setAborted();
+	}
+	else if (running == Waypoint::Action::PREEMPTED) {
+		ROS_INFO_STREAM(action_name_ << ": Preempted by action client");
+		as_.setPreempted();
+	}
+	else if (running == Waypoint::Action::ABORTED) {
+		ROS_INFO_STREAM(action_name_ << ": Aborted by HLP/safety pilot");
+		as_.setAborted();
+	}
+	else if (running == Waypoint::Action::SUCCEEDED) {
+		ROS_INFO_STREAM(action_name_ << ": Waypoint navigation completed");
+		as_.setSucceeded(result_);
+	}
+}
+
+int WaypointGPSActionServer::verifyGeofence() {
+	// TODO: implement equivalent to Boost geometry "within" function (only available from 1.47)
+	return 0;
+}
+
+Waypoint::Action::action_t WaypointGPSActionServer::verifyGpsWaypoint(
+		const asctec_hlp_comm::WaypointGPSGoalConstPtr& waypt) {
+	// TODO: implement check to validate GPS waypoint (i.e., within geofence)
+	return Waypoint::Action::VALID;
 }
 
 
